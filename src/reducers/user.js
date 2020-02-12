28d9bd886967ecb49434e1b313788af422c95e36
Jitgitbit <rowowolf@gmail.com>
2020-02-12 11:04:28 +0100

    error free initial setup

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,9 +1,9 @@
-import { JWT } from '../actions.js'
+// import { JWT } from '../actions.js'
 
 export default function (state = '', action = {}) {
   switch (action.type) {
-    case JWT:
-          return action.payload
+    // case JWT:
+    //       return action.payload
     default:
       return state
   }
